17f4be8c19534050f12e1e3b2d7abeb426f80e0e
Ralph Blach <chip.from.nc@gmail.com>
2020-01-18 10:00:14 -0700

    this formats the message for the oled bonnet and then creates a lat long for transmission to bluetooth

--- rfm69_sr.py
+++ rfm69_sr.py
@@ -5,7 +5,7 @@ Learn Guide: https://learn.adafruit.com/lora-and-lorawan-for-raspberry-pi
 Author: Brent Rubell for Adafruit Industries
 Autonr: Ralph Blach reworked for my purposes
 you will need to download font5x8.bin from https://github.com/adafruit/Adafruit_CircuitPython_framebuf/raw/master/examples/font5x8.bin
-you could use wget https://github.com/adafruit/Adafruit_CircuitPython_framebuf/raw/master/examples/font5x8.bin
+you could use  wget -O font5x8.bin https://github.com/adafruit/Adafruit_CircuitPython_framebuf/blob/master/examples/font5x8.bin?raw=true
 
 The header is 4 bytes
 byte 0 is the target address,( this machince as the receiver)
@@ -18,6 +18,7 @@ an invalid packet minus the 4 byte header will look like ['kf4wbk', 'V']
 """
 # Import Python System Libraries
 import time
+import os
 # Import Blinka Libraries
 # import the SSD1306 module.
 import adafruit_ssd1306
@@ -55,10 +56,17 @@ gpio22 = DigitalInOut(board.D22)
 gpio22.direction = Direction.INPUT
 gpio22.pull = Pull.UP
 
-
 # Create the I2C interface.
 i2c = busio.I2C(board.SCL, board.SDA)
-
+# this is the degree sign for dosplaying to the display and is specific to the font5x8
+degree_sign_bonnet =  u"\u00f8"
+degree_sign_utf8 = u"\u00b0"
+minutes_sign = u"\u0027"
+if not os.path.exists('font5x8.bin'):
+    print('the file font5x8.bin is not present in the current directory.')
+    print('use the command wget -O font5x8.bin \
+    https://github.com/adafruit/Adafruit_CircuitPython_framebuf/blob/master/examples/font5x8.bin?raw=true to download')
+    exit(-1)
 # 128x32 OLED Display
 display = adafruit_ssd1306.SSD1306_I2C(128, 32, i2c, addr=0x3c)
 
@@ -72,24 +80,18 @@ height = display.height
 CS = DigitalInOut(board.CE1)
 RESET = DigitalInOut(board.D25)
 spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
-#rfm69 = adafruit_rfm69.RFM69(spi, CS, RESET, 434.0, sync_word=b'\x2d\xd4')
-rfm69 = adafruit_rfm69.RFM69(spi, CS, RESET, 433.0)
+rfm69 = adafruit_rfm69.RFM69(spi, CS, RESET, 433.0, sync_word=b'\x2D\xD4')
 prev_packet = None
-# Optionally set an encryption key (16 byte AES key). MUST match both
-# on the transmitter and receiver (or be set to None to disable/the default).
-#rfm69.encryption_key = b'\x01\x02\x03\x04\x05\x06\x07\x08\x01\x02\x03\x04\x05\x06\x07\x08'
 
 display.fill(0)
 display.text('Remote Location', 0, 0, 1)
 display.show()
-counter = 0
-while True:
 
+while True:
     packet = None
     # draw a box to clear the image
     # check for packet rx
     packet = rfm69.receive(with_header=True, rx_filter=1)
-    # print('packet={}'.format(packet))
     if packet is not None:
         display.fill(0)
         display.text('Remote Location', 0, 0, 1)
@@ -109,21 +111,24 @@ while True:
         lat_degrees = packet_list[LATITUDE][:2]
         lat_minutes_seconds = packet_list[LATITUDE][2:]
         north_south = '' if packet_list[LATITUDE_NS] == 'N' else '-'
-        latitude = 'lat = ' + north_south + lat_degrees + " " +lat_minutes_seconds
+        latitude = 'lat = ' + north_south + lat_degrees + degree_sign_bonnet + lat_minutes_seconds + minutes_sign
+        latitude_to_bluetooth = north_south + lat_degrees + degree_sign_utf8 + lat_minutes_seconds + minutes_sign
         # longitude has teh form Longitude (DDDmm.mm)
         long_degrees = packet_list[LONGITUDE][:3]
         long_minutes_seconds = packet_list[LONGITUDE][3:]
 
         east_west = '' if packet_list[LONGITUDE_EW] == 'E' else '-'
-        longitude = 'log = ' + east_west + long_degrees + " " + long_minutes_seconds
+        longitude = 'log = ' + east_west + long_degrees + degree_sign_bonnet + long_minutes_seconds + minutes_sign
+        longitude_to_bluetooth = east_west + long_degrees + degree_sign_utf8 + long_minutes_seconds + minutes_sign
         display.text(latitude, 0, 8, 1)
         display.text(longitude, 0, 16, 1)
-        display.text('valid, {}'.format(callsign),0, 24, 1)
+        display.text('valid, {} {:01x}'.format(callsign, header[2] & 0xf),0, 24, 1)
         ack_data = bytes('a','utf-8')
         # create of tuple of to, from, id, status,
         send_tuple=(header[1], header[0], header[2], 0x80)
 
         rfm69.send(ack_data, tx_header=send_tuple)
+        print ('{} {}'.format(latitude_to_bluetooth, longitude_to_bluetooth))
         time.sleep(1)
 
     if not btnA.value:
