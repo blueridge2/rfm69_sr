bfc319f93c4ca728fe3404ed7fc67669b02019ec
Ralph Blach <chip.from.nc@gmail.com>
2023-09-07 08:52:25 -0400

    new file for logging

---
+++ logging.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# Copyright 2023 Ralph Carl Blach III
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute,
+# sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
+# subject to the following conditions:
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+# OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR
+# ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH
+# THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+import datetime
+import threading
+
+
+class Logging(object):
+    """
+    This is a simple logger for the rfm radio
+    """
+    def __init__(self):
+        """
+        The init class for the lock and location
+        """
+        self.__lock = threading.Lock()
+
+    def log(self,  data: str = None):
+        """
+        Log the screen to the output
+
+        """
+
+        data = '' if data is None else data
+        self.__lock.acquire()
+        current_time = datetime.datetime.now()
+        string_time = current_time.strftime("%y:%b:%H:%M:%S")
+        print(string_time + " " + data)
+        self.__lock.release()
+
+
